b2ea03d5945072347dddf2b536e98ec4e49f826e
Jesse <nguyenjessev@gmail.com>
2021-01-31 07:16:24 -1000

    Install application gems and dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :twoots
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "twoots#index"
 end
